8644a7624d8f62ca429c93736d29fb920965d34c
Enkhnasan03 <enkhush.c@gmail.com>
2025-10-22 23:13:27 +0800

    Test

--- src/2DShape.cpp
+++ src/2DShape.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+class Shape{
 
+};
+
+class 2DShape : public Shape{
+
+};
+class Circle : public 2DShape{
+
+};
+class Square : public 2DShape{
+
+};
+class Triangle : public 2DShape{
+
+};
+
+int main(){
+    
+}
