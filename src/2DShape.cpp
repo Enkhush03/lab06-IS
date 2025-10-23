e3c82f292100f98b654137452f1f9554585ae3e7
Nomin-Erdene Erdenekhuyag <23b1num2195@stud.num.edu.mn>
2025-10-23 16:18:58 +0800

    l

--- src/2DShape.cpp
+++ src/2DShape.cpp
@@ -8,6 +8,11 @@ class 2DShape : public Shape{
 
 };
 class Circle : public 2DShape{
+private: 
+double x, y;
+double r;
+public: circle( double cx, double cy, double r):2DShape
+
 
 };
 class Square : public 2DShape{
