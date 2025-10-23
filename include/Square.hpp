699eca2aa9f84f77ba31b78fa0c6fdd57642111d
Shagai-123 <mungunshagaishiirev@gmail.com>
2025-10-23 19:57:21 +0800

    square

--- include/Square.hpp
+++ include/Square.hpp
@@ -1,1 +1,17 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include "Shape.h"
+
+class Square : public Shape {
+private:
+    double side;
+
+public:
+    Square(double s);
+    double getArea() const override;
+    double getPerimeter() const override;
+};
+
+#endif
 
